1e806d45aaeb48c19f0f0f7edd82d846ca1682b5
noxxxxxious <sean.fried@gmail.com>
2023-04-29 12:48:32 -0500

    Set max width on dashboard sections

--- backend/index.ts
+++ backend/index.ts
@@ -3,9 +3,13 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('Hello World!');
 })
+
+// app.get('/api/organizations', (req, res) => {
+
+// })
 
 app.listen(port, () => {
   console.log(`Express app listening on port ${port}!`);
